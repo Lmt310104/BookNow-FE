15212fc42db1366f06b09a97d3fac2765ec9aad4
DuyThong28 <duythong28112004@gmail.com>
2024-10-14 04:10:49 +0700

    create 26 files, update 26 files and delete 3 files

--- src/app/routes/auth/sign-in.tsx
+++ src/app/routes/auth/sign-in.tsx
@@ -1,3 +1,53 @@
-export const SignInRoute = () => {
-  return <div>SignInRoute</div>;
-};
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
+export function SignInRoute() {
+  return (
+    <div className="w-full grid grid-cols-2 h-screen">
+      <div className="bg-black"></div>
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Dang Nhap</h1>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Mat khau</Label>
+              <Input id="password" type="password" required />
+            </div>
+            <a
+              href="./forgot-password"
+              className="ml-auto inline-block text-sm underline"
+            >
+              Quen mat khau?
+            </a>
+            <Button className="w-full" type="submit">
+              <Link to="/dashboad">Dang Nhap</Link>
+            </Button>
+            <Button variant="outline" className="w-full">
+              Dang Nhap voi Google
+            </Button>
+          </div>
+
+          <div className="mt-4 text-center text-sm">
+            Chua co tai khoan?{" "}
+            <a href="./sign-up" className="underline">
+              Dang Ky
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/routes/customers/account-address.tsx
+++ src/app/routes/customers/account-address.tsx
@@ -1,4 +1,91 @@
+import { Button } from "@/components/ui/button";
+
+import { PlusCircle } from "lucide-react";
+
+import CustomerLayout from "@/components/layouts/customer-layout";
+
 export const AccountAddressRoute = () => {
-    return <div>AccountAddressRoute</div>;
-  };
-  
+  return (
+    <CustomerLayout>
+      <main className="flex flex-1 flex-col gap-6 py-6 pl-6 bg-[#f9f9f9] overflow-y-auto">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">Dia Chi Cua Toi</h1>
+          <div className="ml-auto flex items-center gap-2">
+            <Button className="gap-1">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Them dia chi moi
+              </span>
+            </Button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="  flex flex-row justify-between rounded-lg border border-dashed shadow-sm w-full bg-white p-4">
+            <div className="flex flex-col gap-1">
+              <div>DAO DUY THONG</div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dia chi:</span> đường Hàn
+                Thuyên, Phường Linh Trung, Thành phố Thủ Đức, Hồ Chí Minh
+              </div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dien thoai:</span> 0343800708
+              </div>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+              <Button variant="secondary">Chinh sua</Button>
+              <Button variant="outline">Xoa</Button>
+            </div>
+          </div>
+          <div className="  flex flex-row justify-between rounded-lg border border-dashed shadow-sm w-full bg-white p-4">
+            <div className="flex flex-col gap-1">
+              <div>DAO DUY THONG</div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dia chi:</span> đường Hàn
+                Thuyên, Phường Linh Trung, Thành phố Thủ Đức, Hồ Chí Minh
+              </div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dien thoai:</span> 0343800708
+              </div>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+              <Button variant="secondary">Chinh sua</Button>
+              <Button variant="outline">Xoa</Button>
+            </div>
+          </div>{" "}
+          <div className="  flex flex-row justify-between rounded-lg border border-dashed shadow-sm w-full bg-white p-4">
+            <div className="flex flex-col gap-1">
+              <div>DAO DUY THONG</div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dia chi:</span> đường Hàn
+                Thuyên, Phường Linh Trung, Thành phố Thủ Đức, Hồ Chí Minh
+              </div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dien thoai:</span> 0343800708
+              </div>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+              <Button variant="secondary">Chinh sua</Button>
+              <Button variant="outline">Xoa</Button>
+            </div>
+          </div>{" "}
+          <div className="  flex flex-row justify-between rounded-lg border border-dashed shadow-sm w-full bg-white p-4">
+            <div className="flex flex-col gap-1">
+              <div>DAO DUY THONG</div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dia chi:</span> đường Hàn
+                Thuyên, Phường Linh Trung, Thành phố Thủ Đức, Hồ Chí Minh
+              </div>
+              <div className="text-sm">
+                <span className="text-[#787C80]">Dien thoai:</span> 0343800708
+              </div>
+            </div>
+            <div className="flex flex-row gap-4 items-center">
+              <Button variant="secondary">Chinh sua</Button>
+              <Button variant="outline">Xoa</Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </CustomerLayout>
+  );
+};
