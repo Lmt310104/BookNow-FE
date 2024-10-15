cea3a1b4a4cba5e3001ecce1bc010b3c87579e1f
Lmt310104 <lmtoan311@gmail.com>
2024-10-15 21:55:35 +0700

    feat: verification email

--- src/app/routes/auth/verification/verification.tsx
+++ src/app/routes/auth/verification/verification.tsx
@@ -1,7 +1,29 @@
 import { Button } from "@/components/ui/button";
+import useVerificationEmail from "@/features/auth/apis/verification";
 import { MdMarkEmailUnread } from "react-icons/md";
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 export const VerificationRoute = () => {
+    const navigate = useNavigate()
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const token = params.get('token');
+    const verificationEmail = useVerificationEmail({
+        mutationConfig: {
+            onSuccess: () => {
+                navigate("/verification/success")
+            },
+            onError: () => {
+                navigate("/verification/failed")
+            }
+        }
+    })
+    const handleClickVerify = () => {
+        if (token) {
+            verificationEmail.mutate({ token });
+        } else {
+            console.error("Token is null");
+        }
+    }
     return (
         <div className="flex flex-col justify-center items-center h-[100vh] gap-10">
             <MdMarkEmailUnread size={48} color="#4caf50"/>
@@ -9,7 +31,7 @@ export const VerificationRoute = () => {
             <span className="font-bold">You're already have a BookNow account</span>
             <div className="flex flex-col items-center gap-5">
                 <span>To continue:</span>
-                <Button className="h-10"><Link to="success">Verify your email here</Link></Button>
+                <Button className="h-10" onClick={handleClickVerify}>Verify your email here</Button>
             </div>
         </div>
     )
--- src/app/routes/auth/verification/verification_failed.tsx
+++ src/app/routes/auth/verification/verification_failed.tsx
@@ -1,9 +1,12 @@
 export const VerificationFailed = () => {
     return (
-        <>
-            <h1>Verification Failed</h1>
-            <span>Sorry, we couldn't verify your email.</span>
-            <span>Please try again later.</span>
-        </>
+        <div className="flex flex-col h-[100vh] items-center justify-center gap-10">
+            <h1 className="text-2xl font-bold text-red-800">Verification Failed</h1>
+            <div className="flex flex-col items-center gap-5">
+                <span>Sorry, we couldn't verify your email.</span>
+                <span>Please try again later.</span>
+            </div>
+            
+        </div>
     )
 }
