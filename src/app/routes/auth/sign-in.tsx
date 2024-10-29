1adfed87a4fcb87bed3da5e854f3972ca09c2fca
DuyThong28 <duythong28112004@gmail.com>
2024-10-29 12:36:59 +0700

    feat: sign in with phone

--- src/app/routes/auth/sign-in.tsx
+++ src/app/routes/auth/sign-in.tsx
@@ -8,21 +8,15 @@ import { jwtDecode } from "jwt-decode";
 import { JWTDecode } from "@/context/auth";
 import { UserRole } from "@/common/enums";
 import useAuth from "@/hooks/useAuth";
-import { api, setAccessToken } from "@/lib/api-client";
+import { setAccessToken } from "@/lib/api-client";
 const URL_SERVER = import.meta.env.VITE_URL_SERVER;
 
 export default function SignInRoute() {
-  const [input, setinput] = useState({ email: "", password: "" });
+  const [input, setinput] = useState({ email_phone: "", password: "" });
   const [auth, setAuth] = useAuth();
   const navigate = useNavigate();
 
-  const handleChangeInput = ({
-    name,
-    value,
-  }: {
-    name: string;
-    value: string;
-  }) => {
+  const handleChangeInput = ({name,value,}: {name: string;value: string;}) => {
     setinput((currentInfo) => {
       const newInfo = {
         ...currentInfo,
@@ -35,11 +29,17 @@ export default function SignInRoute() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await authService.signInWithEmail(input);
+      let response;
+      if (/^\d+$/.test(input.email_phone)) {
+        response = await authService.singInWithPhone(input);
+      } else {
+        response = await authService.signInWithEmail(input);
+      }
+
       if (response.data) {
         const accessToken: string = response.data.access_token;
         setAccessToken(accessToken);
-        setinput({ email: "", password: "" });
+        setinput({ email_phone: "", password: "" });
         const { id, role }: JWTDecode = jwtDecode(accessToken);
         setAuth({
           userId: id,
@@ -75,16 +75,16 @@ export default function SignInRoute() {
           </div>
           <form className="grid gap-4" onSubmit={handleSubmit}>
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="phone-email">Email hoac So dien thoai</Label>
               <Input
-                id="email"
-                type="email"
-                name="email"
-                placeholder="email@example.com"
+                id="phone-email"
+                type="text"
+                name="phone-email"
+                placeholder="Email hoac So dien thoai"
                 required
-                value={input.email}
+                value={input.email_phone}
                 onChange={(e) =>
-                  handleChangeInput({ name: "email", value: e.target.value })
+                  handleChangeInput({ name: "email_phone", value: e.target.value })
                 }
               />
             </div>
